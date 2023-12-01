b59ffcf93379bf28416e87c0a026da4d49cfcb52
Alex Maltsev <and1sscsgo@gmail.com>
2023-12-01 03:06:25 +0200

    Removed incorrectly optimized memset optimization.

--- source/lib/memory_util.c
+++ source/lib/memory_util.c
@@ -1,28 +1,18 @@
 #include "memory_util.h"
 
+// TODO: optimize these procedures, so we don't copy byte by byte, but copy
+//       whole cache lines instead
+
 void* memset(void* dst, u8 val, u64 len) {
     u8* _dst = (u8*) dst;
-    u8* _rem_dst = _dst + len % sizeof(u64);
 
-    while (_dst < _rem_dst) {
-        *_dst++ = val;
-    }
-
-    u16 wval = ((u16) val << 8) | val;
-    u32 dwval = ((u32) wval << 16) | wval;
-    u64 qwval = ((u64) dwval << 32) | dwval;
-
-    u64* _ldst = (u64*) _dst;
-    u64* end = (u64*) ((u64) dst + len);
-    while (_ldst < end) {
-        *_ldst++ = qwval;
+    for (u64 i = 0; i < len; i++) {
+        _dst[i] = val;
     }
 
     return dst;
 }
 
-// TODO: optimize this, so we don't copy byte by byte, but copy whole cache
-//       lines instead
 void memcpy(void* dst, void* src, u64 len) {
     u8* _dst = (u8*) dst;
     u8* _src = (u8*) src;
