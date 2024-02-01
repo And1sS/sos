e053cd02efa0e8245478eb4c8189c4b504c52307
Alex Maltsev <and1sscsgo@gmail.com>
2024-02-01 21:16:50 +0200

    Guarded vmm with lock.

--- source/kernel/memory/virtual/vmm.c
+++ source/kernel/memory/virtual/vmm.c
@@ -7,6 +7,7 @@ vm_space kernel_vm_space;
 // TODO: make this percpu
 // can be changed without locking as this is confined to current cpu
 static vm_space* current_vm_space = NULL;
+static lock vmm_lock = SPIN_LOCK_STATIC_INITIALIZER;
 
 void vmm_init() {
     arch_init_kernel_vm(&kernel_vm_space);
@@ -18,11 +19,18 @@ void vmm_init() {
 
 vm_space* vmm_kernel_vm_space() { return &kernel_vm_space; }
 
-vm_space* vmm_current_vm_space() { return current_vm_space; }
+vm_space* vmm_current_vm_space() {
+    bool interrupts_enabled = spin_lock_irq_save(&vmm_lock);
+    vm_space* result = current_vm_space;
+    spin_unlock_irq_restore(&vmm_lock, interrupts_enabled);
+    return result;
+}
 
 void vmm_set_vm_space(vm_space* space) {
+    bool interrupts_enabled = spin_lock_irq_save(&vmm_lock);
     atomic_set((u64*) current_vm_space, (u64) space);
     arch_set_vm_space(space);
+    spin_unlock_irq_restore(&vmm_lock, interrupts_enabled);
     // no need to call notify here, since arch already knows that vm space has
     // changed
 }
