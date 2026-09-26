8f5a465851df8f4453d9b835a58d60add56ec524
Alex Maltsev <and1sscsgo@gmail.com>
2026-09-26 20:56:11 +0300

    Added READ_ONCE, WRITE_ONCE macros. These are backports from add-vfs branch.

--- source/kernel/synchronization/atomics.h
+++ source/kernel/synchronization/atomics.h
@@ -3,6 +3,15 @@
 
 #include "../lib/types.h"
 
+// These macros provide atomic, single-copy load/store semantics
+#define ACCESS_ONCE(x) (*(volatile typeof(x) *)&(x))
+
+#define READ_ONCE(x) \
+({ typeof(x) ___x = ACCESS_ONCE(x); ___x; })
+
+#define WRITE_ONCE(x, val) \
+do { ACCESS_ONCE(x) = (val); } while (0)
+
 // has read-acquire semantics
 extern u64 atomic_exchange(volatile u64* addr, volatile u64 new_value);
 
